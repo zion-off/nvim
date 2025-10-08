44ac1874cbabacb30fd56fdee6e5225319ce355b
zion <zion@nyu.edu>
2025-10-09 02:52:45 +0600

    login to okto using gh cli

--- lua/plugins/plugins.lua
+++ lua/plugins/plugins.lua
@@ -223,4 +223,18 @@ return {
       },
     },
   },
+  {
+    "pwntester/octo.nvim",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-telescope/telescope.nvim",
+      "nvim-tree/nvim-web-devicons",
+    },
+    opts = {
+      enable_builtin = true,
+      default_to_projects_v2 = false,
+      default_merge_method = "squash",
+      gh_cmd = "/opt/homebrew/bin/gh",
+    },
+  },
 }
