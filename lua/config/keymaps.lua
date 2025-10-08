056f05cf91c75069e5aba5507bb47a18255517ce
zion <zion@nyu.edu>
2025-10-09 04:48:05 +0600

    add shell command shortcut

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,11 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- Execute shell command silently
+vim.keymap.set("n", "<leader>xs", function()
+  local cmd = vim.fn.input("Command: ")
+  if cmd ~= "" then
+    vim.fn.system(cmd)
+  end
+end, { desc = "Execute shell command silently" })
