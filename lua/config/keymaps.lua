f6ffa2e46dfbab74ced9275f62b82c87e3f9d206
zion <ajz317@nyu.edu>
2025-10-10 01:55:59 +0600

    add claude keybindings

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,4 +1,5 @@
 -- Keymaps are automatically loaded on the VeryLazy event
+-- 
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
@@ -11,6 +12,9 @@ vim.keymap.set("n", "<leader>xs", function()
 end, { desc = "Execute shell command" })
 
 vim.keymap.set("n", "<leader>ac", ":ClaudeCode<CR>", { desc = "Toggle Claude Code" })
+vim.keymap.set("v", "<leader>as", "<cmd>ClaudeCodeSend<cr>", { desc = "Send to Claude" })
+vim.keymap.set("n", "<leader>ay", "<cmd>ClaudeCodeDiffAccept<cr>", { desc = "Accept diff" })
+vim.keymap.set("n", "<leader>an", "<cmd>ClaudeCodeDiffDeny<cr>", { desc = "Deny diff" })
 
 -- Override terminal keybindings to open in current buffer
 vim.keymap.set("n", "<leader>ft", ":terminal<CR>", { desc = "Terminal (current buffer)" })
