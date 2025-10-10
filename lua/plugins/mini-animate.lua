0e89c4eb7f23eaba00eaa98cd1e3bed996fa8283
zion <zion@nyu.edu>
2025-10-10 19:02:31 +0600

    add cursor animation

---
+++ lua/plugins/mini-animate.lua
@@ -0,0 +1,20 @@
+return {
+  {
+    "nvim-mini/mini.animate",
+    event = "VeryLazy",
+    opts = {
+      scroll = {
+        enable = false
+      },
+      resize = {
+        enable = false
+      },
+      open = {
+        enable = false
+      },
+      close = {
+        enable = false
+      }
+    },
+  },
+}
