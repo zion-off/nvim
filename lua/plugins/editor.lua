3ff734ac97d734de8982c748a878e467059eaa89
zion <zion@nyu.edu>
2025-10-19 14:41:01 +0600

    remove cursor smear

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -62,27 +62,4 @@ return {
       },
     },
   },
-  -- Cursor animation
-  {
-    "sphamba/smear-cursor.nvim",
-
-    opts = {
-      -- Cursor color
-      cursor_color = "#c6c6c6",
-
-      -- Smear cursor when switching buffers or windows.
-      smear_between_buffers = true,
-
-      -- Smear cursor when moving within line or to neighbor lines.
-      -- Use `min_horizontal_distance_smear` and `min_vertical_distance_smear` for finer control
-      smear_between_neighbor_lines = true,
-
-      -- Draw the smear in buffer space instead of screen space when scrolling
-      scroll_buffer_space = true,
-
-      -- Set to `true` if your font supports legacy computing symbols (block unicode symbols).
-      -- Smears will blend better on all backgrounds.
-      legacy_computing_symbols_support = false,
-    },
-  },
 }
