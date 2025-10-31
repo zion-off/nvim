9ff845086f8b91696be4c1ecf59643ba36fb7394
zion <zion@nyu.edu>
2025-10-31 20:40:34 +0600

    disable auto change directory

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,7 +1,6 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
-vim.opt.autochdir = true
 vim.opt.spell = false
 vim.opt.autowriteall = true -- Auto-save files
 vim.opt.background = "dark"
