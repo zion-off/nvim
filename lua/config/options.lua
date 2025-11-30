50ed20d58f4bb35fa53be5cb981021bb1954c1cd
zion <zion@nyu.edu>
2025-11-30 21:56:59 +0600

    fix double ghost text from copilot

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,6 +1,9 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+-- Disable AI in completion menu, use inline ghost text instead
+vim.g.ai_cmp = false
 vim.opt.spell = false
 vim.opt.autowriteall = true -- Auto-save files
 vim.opt.background = "dark"
