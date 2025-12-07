809d21358157dedf317d7cfe672773248e39f219
zion <zion@nyu.edu>
2025-12-07 20:43:46 +0600

    setup neovide

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -14,5 +14,29 @@ vim.opt.timeoutlen = 400
 vim.opt.wrap = true
 vim.opt.breakindent = true
 
+-- Neovide settings
+if vim.g.neovide then
+  -- Load environment from interactive shell (reads .zshrc)
+  local shell_env = vim.fn.system("zsh -ic 'printenv'")
+  for line in shell_env:gmatch("[^\n]+") do
+    local key, value = line:match("^([^=]+)=(.*)$")
+    if key and value then
+      vim.env[key] = value
+    end
+  end
+
+  vim.o.guifont = "GeistMono Nerd Font:h13"
+  vim.g.neovide_fullscreen = true
+  vim.g.neovide_cursor_animation_length = 0
+  vim.g.neovide_cursor_trail_size = 0
+  vim.g.neovide_cursor_vfx_mode = ""
+  vim.g.neovide_scroll_animation_length = 0 -- Disable scroll animation to prevent jitter
+
+  -- Cmd+V paste in all modes
+  vim.keymap.set({ "n", "v", "i", "c", "t" }, "<D-v>", function()
+    vim.api.nvim_paste(vim.fn.getreg("+"), true, -1)
+  end, { noremap = true, silent = true })
+end
+
 -- Load terminal colors
 require("config.colors").setup()
