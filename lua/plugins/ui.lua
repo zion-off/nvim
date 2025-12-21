d3c327ffe4e64eedb160c20976b8d00042ab2be1
zion <ajz317@nyu.edu>
2025-12-22 04:11:23 +0600

    remove extraneous themes

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -1,39 +1,4 @@
 return {
-  -- OneDarkPro theme
-  {
-    "olimorris/onedarkpro.nvim",
-    priority = 0,
-    config = function()
-      require("onedarkpro").setup({
-        colors = {
-          onedark = {
-            bg = "#1e2127",
-          },
-          onedark_dark = {
-            bg = "#0f0f0f",
-          },
-        },
-        highlights = {
-          TermCursor = { bg = "#e8e8e8" },
-        },
-        options = {
-          transparency = false,
-        },
-      })
-      -- vim.cmd("colorscheme onedark")
-    end,
-  },
-
-  -- Oxocarbon theme
-  {
-    "nyoom-engineering/oxocarbon.nvim",
-    -- lazy = false,
-    priority = 0,
-    config = function()
-      -- vim.cmd("colorscheme oxocarbon")
-    end,
-  },
-
   -- Jellybeans theme
   {
     "metalelf0/jellybeans-nvim",
