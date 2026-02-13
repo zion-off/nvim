21f3c118ff16261f2a81d2006f7319cd1cec88a5
zion <ajz317@nyu.edu>
2026-02-13 06:06:32 +0600

    show lsp diagnostics in the bufferline

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -2,4 +2,10 @@ return {
   "akinsho/bufferline.nvim",
   version = "*",
   dependencies = "nvim-tree/nvim-web-devicons",
+  opts = {
+    options = {
+      diagnostics = "nvim_lsp",
+      show_buffer_close_icons = false,
+    },
+  },
 }
