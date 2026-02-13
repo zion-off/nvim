4de593831d9597742bb0cd219a54338f40f12e83
zion <zion@nyu.edu>
2026-02-14 05:14:08 +0600

    configure octo.nvim

--- lua/plugins/tools.lua
+++ lua/plugins/tools.lua
@@ -9,9 +9,10 @@ return {
     },
     opts = {
       picker = "telescope",
+      use_local_fs = true,
       enable_builtin = true,
       default_to_projects_v2 = false,
-      default_merge_method = "squash",
+      default_merge_method = "rebase",
       gh_cmd = "/opt/homebrew/bin/gh",
     },
   },
