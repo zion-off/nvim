fc32c042cc7ab7753ade24e847b5a8d9ab2bd563
zion <zion@nyu.edu>
2026-02-14 05:05:38 +0600

    add env variable for loading mole.nvim locally

--- lua/plugins/mole.lua
+++ lua/plugins/mole.lua
@@ -1,5 +1,16 @@
-return {
-  "zion-off/mole.nvim",
+local use_local = vim.env.LOCAL_MOLE_NVIM or os.getenv("LOCAL_MOLE_NVIM")
+
+local plugin_spec = {
   dependencies = { "MunifTanjim/nui.nvim" },
-  opts = {},
+  opts = {
+    picker = "snacks",
+  },
 }
+
+if use_local then
+  plugin_spec.dir = "~/projects/mole.nvim"
+else
+  plugin_spec[1] = "zion-off/mole.nvim"
+end
+
+return plugin_spec
