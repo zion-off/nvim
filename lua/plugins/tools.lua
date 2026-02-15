8021685cc82b91c4574e16f9a480838010ddaf34
zion <zion@nyu.edu>
2026-02-15 18:40:08 +0600

    configure Diffview merge tool

--- lua/plugins/tools.lua
+++ lua/plugins/tools.lua
@@ -46,6 +46,13 @@ return {
       { "<leader>gF", "<cmd>DiffviewFileHistory<cr>", desc = "Diffview branch history" },
       { "<leader>gq", "<cmd>DiffviewClose<cr>", desc = "Diffview close" },
     },
+    opts = {
+      view = {
+        merge_tool = {
+          layout = "diff3_mixed",
+        },
+      },
+    },
   },
   -- Claude Code integration
   {
